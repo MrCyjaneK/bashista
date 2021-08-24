4fcd362b732d3a1b9819cf0f4c59efc9c8a2cbb3
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-08-24 13:25:33 +0200

    use time as a seed

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/sqweek/dialog"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	ok := true
 	for ok {
 		ok = dialog.Message("%s", getQuote()).Title("Here goes your quote").YesNo()
